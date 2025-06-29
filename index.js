e65b04eef679dfd4c39d091cd2bd7f2c097f15b9
victorMwatu <victormwatu@gmail.com>
2025-06-29 23:51:49 +0300

    complete algorithms has target sum lab

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n**2)
 */
 
 /* 
   Add your pseudocode here
+add each number with each other and check if it is equal to target
 */
 
 /*
